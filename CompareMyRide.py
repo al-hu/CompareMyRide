60d61deadcfdaff6e5230956ca1f01b78234e577
Albert <al.hu@berkeley.edu>
2016-09-12 17:13:17 -0700

    Completed, everything works

--- CompareMyRide.py
+++ CompareMyRide.py
@@ -12,7 +12,7 @@ lyft_client_token = "gAAAAABX1vxNONlyqn0Id7Rv-WyZPvNAq_u-z5lhSghC0dAZnCUv-_snT1b
 lyft_client_secret = "3BP5tUBf8N3IWdfltvgdWWLSpKorn6JM"
 
 
-uber_url = "https://api.uber.com/v1/estimates/price"
+uber_base_url = "https://api.uber.com/v1/estimates/price"
 lyft_url = "https://api.lyft.com/v1/cost"
 lyft_auth_url = "https://api.lyft.com/oauth/token"
 
@@ -47,16 +47,15 @@ def get_uber_estimates(start_lat, start_lon, end_lat, end_lon):
         'end_latitude': end_lat,
         'end_longitude': end_lon,
     }
-    uber_response = requests.get(uber_url, params=uber_parameters)
+    uber_response = requests.get(uber_base_url, params=uber_parameters)
 
     if uber_response.status_code == 422:
         print("Your distance exceeds 100 miles.  Please choose locations that are less than 100 miles apart.")
         quit()
     else:
         uber_estimates = uber_response.json()
-        uber_price_estimate = uber_estimates['prices'][0]['estimate']
-        uber_time_estimate = uber_estimates['prices'][0]['duration'] / 60
-        return [uber_price_estimate, uber_time_estimate]
+        return uber_estimates
+
 
 def get_lyft_access_token():
 
@@ -73,23 +72,68 @@ def get_lyft_access_token():
         return lyft_auth_token
 
 
+def get_lyft_estimates(auth_token):
+    headers = {'Content-Type': 'application/json', 'Authorization': 'bearer ' + auth_token}
     lyft_parameters = {
-        'start_latitude': start_lat,
-        'start_longitude': start_lon,
-        'end_latitude': end_lat,
-        'end_longitude': end_lon,
+        'start_lat': start_lat,
+        'start_lng': start_lon,
+        'end_lat': end_lat,
+        'end_lng': end_lon,
     }
+    lyft_response = requests.get(lyft_url, params = lyft_parameters, headers = headers)
 
+    if lyft_response.status_code != 200:
+        print("Error: " + str(response.status_code))
+        quit()
+    else:
+        lyft_estimates = lyft_response.json()
+        return lyft_estimates
 
+def print_results(uber_estimates = None, lyft_estimates = None):
+    if uber_estimates == None and lyft_estimates == None:
+        print("There are no available rides, sorry.")
+        quit()
+    else:
+        number_results_uber = len(uber_estimates['prices'])
+        number_results_lyft = len(lyft_estimates['cost_estimates'])
 
+        print("Estimates for Uber:")
+        for x in range(0, number_results_uber):
+            curr = uber_estimates['prices'][x]
+            travel_time = curr['duration'] / 60
 
+            print("Product: " + curr['display_name'])
+            print("Estimated Cost: " + curr['estimate'])
+            print("Estimated Travel Time: " + str(travel_time) + " minutes")
+            print("\n")
+
+        print("-------------------------------")
+
+        for x in range(0, number_results_lyft):
+            curr = lyft_estimates['cost_estimates'][x]
+            min_cost = curr['estimated_cost_cents_min'] / 60
+            max_cost = curr['estimated_cost_cents_max'] / 60
+            travel_time = curr['estimated_duration_seconds'] / 60
+            estimate_range = "$" + str(min_cost) + "-" + str(max_cost)
+            if min_cost == max_cost:
+                estimate_range = "$" + str(min_cost)
+            print("Product: " + curr['ride_type'])
+            print("Estimated Cost: " + estimate_range)
+            print("Estimated Travel Time: " + str(travel_time) + " minutes")
+            print("\n")
 
 if __name__ == "__main__":
-    uber_response = get_uber_estimates(start_lat, start_lon, end_lat, end_lon)
+    uber_estimates = get_uber_estimates(start_lat, start_lon, end_lat, end_lon)
     access_token = get_lyft_access_token()
+    lyft_estimates = get_lyft_estimates(access_token)
+    print_results(uber_estimates, lyft_estimates)
 
-    print("Calculating estimates for Uber and Lyft from " + str(start_lat) + ", " + str(start_lon) + " to " + str(end_lat) + ", " +str(end_lon) + ":")
-    print("Uber: Estimated Cost is " + str(uber_response[0]) + ".")
-    print("Uber: Estimated Time is " + str(uber_response[1]) + " minutes.")
-    print("Lyft: Estimated Cost is " + str(lyft_price_estimate) + ".")
-    print("Lyft: Estimated Cost is " + str(lyft_time_estimate) + " minutes.")
+    # print("Calculating estimates for Uber and Lyft from " + str(start_lat) + ", " + str(start_lon) + " to " + str(end_lat) + ", " +str(end_lon) + ":")
+    # print("Uber: Estimated Cost is " + str(uber_estimates[0]) + ".")
+    # print("Uber: Estimated Time is " + str(uber_estimates[1]) + " minutes.")
+    # print("Lyft: Estimated Cost is " + str(lyft_price_estimate) + ".")
+    # print("Lyft: Estimated Cost is " + str(lyft_time_estimate) + " minutes.")
+
+    # uber_price_estimate = uber_estimates['prices'][0]['estimate']
+    # uber_time_estimate = uber_estimates['prices'][0]['duration'] / 60
+    # return [uber_price_estimate, uber_time_estimate]
